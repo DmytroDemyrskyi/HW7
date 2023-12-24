c99f307b8781bb8aded6f7f2a0d6ed5f979015cd
DmytroDemyrskyi <long28469@gmail.com>
2023-12-24 22:20:36 +0200

    first-commit

---
+++ school/forms.py
@@ -0,0 +1,60 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from school.models import Teachers, Groups, Students
+
+
+class TeacherForm(forms.ModelForm):
+    groups = forms.ModelMultipleChoiceField(
+        queryset=Groups.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    class Meta:
+        model = Teachers
+        fields = ["full_name", "date_of_birth", "groups"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields["groups"].initial = self.instance.groups.all()
+
+    def clean_full_name(self):
+        full_name = self.cleaned_data["full_name"]
+        if len(full_name) > 30:
+            raise forms.ValidationError(
+                "ПІБ занадто довге. Максимальна довжина 30 символів."
+            )
+        return full_name
+
+
+class GroupForm(forms.ModelForm):
+    class Meta:
+        model = Groups
+        fields = ["name", "teacher"]
+
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+        if len(name) > 10:
+            raise forms.ValidationError(
+                "Назва занадто довга. Максимальна довжина 10 символів."
+            )
+        return name
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Students
+        fields = ["full_name", "year", "group"]
+
+    def clean_full_name(self):
+        full_name = self.cleaned_data["full_name"]
+
+        if any(char.isdigit() for char in full_name):
+            raise ValidationError("Ім'я не повинно містити цифри.")
+
+        if len(full_name) > 30:
+            raise ValidationError("Довжина імені не повинна перевищувати 30 символів.")
+
+        return full_name
